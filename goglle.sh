f25e822dd2ca52c8941bb2552275052171942dda
MrT1T4Ntzy <91118364+MrT1T4Ntzy@users.noreply.github.com>
2021-09-27 11:40:15 +0700

    Update goglle.sh

--- goglle.sh
+++ goglle.sh
@@ -6,6 +6,6 @@ echo -e "============================="
 echo -e "{•}Author : Mr.T1T4N" 
 echo -e "{•}Team : Surakarta•Cyber•Security"
 echo -e "============================="
-read -p ""Telusuri
+read -p ""Telusuri :
 link="https://www.google.com/$Terulusuri
 curl -s $link
