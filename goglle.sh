3eec9a4e8d92c7645573fccf5f77a0c7998efeda
MrT1T4Ntzy <91118364+MrT1T4Ntzy@users.noreply.github.com>
2021-09-27 11:38:48 +0700

    goggle.sh

---
+++ goglle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#tampilan
+echo -e "SELAMAT DATANG MANUSIA"
+echo -e "============================="
+echo -e "{•}Author : Mr.T1T4N" 
+echo -e "{•}Team : Surakarta•Cyber•Security"
+echo -e "============================="
+read -p ""Telusuri
+link="https://www.google.com/$Terulusuri
+curl -s $link
