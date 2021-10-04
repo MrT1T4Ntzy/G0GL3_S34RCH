59ee61fdf99876b07119b195d93321c474d0479a
MrT1T4Ntzy <91118364+MrT1T4Ntzy@users.noreply.github.com>
2021-10-04 10:05:01 +0700

    Create googgle.sh

---
+++ googgle.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+figlet Google | lolcat
+figlet Search | lolcat
+date | lolcat
+
+sleep 2
+clear
+
+
+echo " ======================================================="
+echo  "Author : Mr.T1T4N"
+echo  "Team : Mafia Teknologi"
+echo " GAK ADA MASTAH"
+echo " ========================================================"
+
+echo "[1] LANGSUNG GAK NIHH??"
+echo "[2] Keluar"
+read "Pilih :"
+if [ $pil = '1' ]
+then
+       curl -sX GET "https:google.com/$Penelusuran:"
+       read -p "" Penelusuran:
+
+then
+ elif [ $pil = '2' ]       
+ figlet TERIMA | lolcat
+ figlet KASIH |lolcat
+ date | lolcat
+         exit
+ fi
+ 
+ done
