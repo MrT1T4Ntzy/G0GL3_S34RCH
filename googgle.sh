3e63c6f3a3086fc0dceff3fc282ae1f412742bd9
MrT1T4Ntzy <91118364+MrT1T4Ntzy@users.noreply.github.com>
2021-10-04 10:21:03 +0700

    Update googgle.sh

--- googgle.sh
+++ googgle.sh
@@ -3,32 +3,37 @@
 figlet Google | lolcat
 figlet Search | lolcat
 date | lolcat
-
 sleep 2
 clear
 
 Ulang='Y'
 while [ $Ulang='Y' ]
 do
-echo " ====================================================="
-echo  "Author : Mr.T1T4N"
-echo  "Team : Mafia Teknologi"
-echo " GAK ADA MASTAH"
-echo " ======================================================"
-
-echo "[1] LANGSUNG GAK NIHH??"
-echo "[2] Keluar"
-echo -n "Cepat NGAB Pilih :"
-read "Pilih :"
-if [ $pil = '1' ]
+echo -e"$kuning""=============================================="
+echo -e"$merah" "Author : Mr.T1T4N"
+echo -e"$merah""Team : Mafia Teknologi
+echo -e"$kuning"=============================================="
+       
+       echo
+       echo -e"kuning" "================"
+       echo -e"$biru" "[1] AyoLAH CONTEK"
+       echo -e"$biru" "[2] Laporkan Ada BUG"
+       echo -e"$kuning" "[00] Keluar/EXIT"
+       echo "  ================="
+echo -n "Masukan Pilihannya NGAB"
+read "Pilih ;"
+if [ $pil = '1' ] || [ $pil = '01' ]
 then
-       curl -sX GET "https:google.com/$Penelusuran:"
-       read -p "" Penelusuran:
-
+       curl -sX GET "https://id.jagreward.com/member/verify-mobile/$Nomor"
+       read -p "" Nomor :
+elif  [ $pil = '2' ] || [ $pil = '02' ]
+then   
+      curl -sX GET "https://wa.me/6281219410786/"
+elif [ $pil = '3' ] || [ $pil = '03' ]
 then
- elif [ $pil = '2' ]       
- figlet TERIMA | lolcat
- figlet KASIH |lolcat
+ 
+ figlet Terima | lolcat
+ figlet Kasih | lolcat
  date | lolcat
          exit
  fi
